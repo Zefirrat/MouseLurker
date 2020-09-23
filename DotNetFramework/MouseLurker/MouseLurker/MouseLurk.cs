dcb20b4897619a6d4dff2bb29dfd82defe996218
Zefirrat <leonidpavlov314@gmail.com>
2020-09-23 19:35:28 +0500

    Separated to .NetFramework and .NetCore realisation

---
+++ DotNetFramework/MouseLurker/MouseLurker/MouseLurk.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Input;
+using MouseKeyboardActivityMonitor;
+using MouseKeyboardActivityMonitor.WinApi;
+using MouseEventArgs = System.Windows.Forms.MouseEventArgs;
+
+namespace Spy.Windows.Lurker
+{
+    public class MouseLurk
+    {
+        public MouseLurk(Action callback)
+        {
+            _callback = callback;
+        }
+
+        public void StartPicking()
+        {
+            GlobalHook = new GlobalHooker();
+            MouseListener = new MouseHookListener(GlobalHook);
+            MouseListener.MouseDownExt += _pick;
+            MouseListener.Start();
+        }
+
+        private MouseHookListener MouseListener { get; set; }
+        private GlobalHooker GlobalHook { get; set; }
+
+        private Action _callback;
+
+        private void _pick(object sender, MouseEventExtArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftCtrl))
+            {
+                e.Handled = true;
+                _callback?.Invoke();
+            }
+        }
+
+        public void StopPicking()
+        {
+            MouseListener.MouseDownExt -= _pick;
+            MouseListener.Stop();
+        }
+    }
+}
